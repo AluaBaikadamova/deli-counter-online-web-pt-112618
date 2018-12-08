18968864a20a6c73b3f5484f91acf2dd691d7fda
Alua Baikadamova <abaikad00@gmail.com>
2018-12-08 18:12:09 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,23 @@
 # Write your code here.
+def line(name[])
+  s="The line is currently:"
+  if (name.length==0) puts "The line is currently empty."
+  else 
+    for i=0 to name.length-1
+    
+    s<< " #{i}. #{name[i]}"
+  end
+  
+end
+
+def take_a_number(katz_deli,name)
+  puts "#{name} #{katz_deli.length}"
+end
+
+def now_serving(name)
+  if (name.length==0) puts "There is nobody waiting to be served!"
+  else
+    name2=name.pop
+  puts "#{name2}"
+end 
+end
